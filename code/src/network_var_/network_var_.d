996b47c46ac715d42123ae27b9e8d85016ec5644
Unknown <JonathanILevi@gmail.com>
2018-11-15 20:35:51 -0600

    Further implementation of NetworkVar; tested to compile.

--- code/src/network_var_/network_var_.d
+++ code/src/network_var_/network_var_.d
@@ -1,11 +1,9 @@
 module loose_.network_var_.network_var_;
-import commonImports;
-
-import std.traits;
 
 
 enum Read	;
 enum Write	;
+enum SplitArray	;
 struct Sync {
 	uint	valueId	;
 }
@@ -23,25 +21,49 @@ template Owner(bool isOwner) {
 ////		mixin("alias "name~"=Read");
 ////}
 mixin template NetworkVar() {
-	static foreach(var; getSymbolsByUDA!(this, Sync))) {
-		enum varName	= __traits(identifier, var)[1..$];
-		enum varId	= getUDAs(var, Sync)[0].valueId;
-		@property auto mixin(varName)() {
-			return mixin("this._"~varName);
-		}
-		static if (hasUDA!(this,Write)) {
-			static if (!is(typeof(mixin("this._networkVar_"~varId~"_changed")))) {
-				bool mixin("this._networkVar_"~varId~"_changed") = false;
+	private import std.traits;
+	private import std.algorithm : countUntil;
+	private import std.conv : to;
+	enum _networkVar_private_varName	= q{__traits(identifier, var)[1..$]};
+	enum _networkVar_private_varId	= q{getUDAs!(var, Sync)[0].valueId};
+	enum _networkVar_private_varIdStr	= _networkVar_private_varId~".to!string";
+	static foreach(var; getSymbolsByUDA!(typeof(this), Sync)) {
+		static assert(__traits(identifier, var)[0]=='_');
+		@property mixin("auto "~mixin(_networkVar_private_varName)~"() {
+			return _"~mixin(_networkVar_private_varName)~";
+		}");
+ 		static if (hasUDA!(var,Write)) {
+			static if (hasUDA!(var,SplitArray)) {
+				mixin("typeof(var) _networkVar_"~mixin(_networkVar_private_varIdStr)~"_added = new typeof(*var.ptr)[0];");
+				mixin("typeof(var) _networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed = new typeof(*var.ptr)[0];");
+				@property mixin("void "~mixin(_networkVar_private_varName)~"_add(typeof(*var.ptr) n) {
+					_"~mixin(_networkVar_private_varName)~"	~= n	;
+					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_added	~= n	;
+				}");
+				@property mixin("void "~mixin(_networkVar_private_varName)~"_remove(typeof(*var.ptr) n) {
+					_"~mixin(_networkVar_private_varName)~".remove(_"~mixin(_networkVar_private_varName)~".countUntil(n));
+					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed	~= n	;
+				}");
 			}
-			@property void mixin(varName)(typeof(Sync) n) {
-				mixin("this._"~varName)	= n	;
-				mixin("this._networkVar_"~varId~"_changed")	= true	;
-			}
+			else {
+				static if (!is(typeof(mixin("_networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed")))) {
+					mixin("bool _networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed = false;");
+				}
+				@property mixin("void "~mixin(_networkVar_private_varName)~"(typeof(var) n) {
+					if ("~mixin(_networkVar_private_varName)~"!=n) {
+						_"~mixin(_networkVar_private_varName)~"	= n	;
+						_networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed	= true	;
+					}
+				}");
+			}	
 		}
 	}
 	void networkVar_update(void delegate(ubyte[]) msg_callback, ubyte[][] msgs) {
+		_networkVar_private_update(this, msg_callback, msgs);
+	}
+	static void _networkVar_private_update(typeof(this) this_, void delegate(ubyte[]) msg_callback, ubyte[][] msgs) {
 		pragma(inline, true) ubyte[] serialize(uint valueId)() {
-			
+			return [0,1,2,5];
 		}
 		pragma(inline, true) void deserialize(ubyte[] msg) {
 			
@@ -50,13 +72,15 @@ mixin template NetworkVar() {
 		foreach (msg; msgs) {
 			deserialize(msg);
 		}
-		static foreach(var; getSymbolsByUDA!(this, Sync))) {
-			static if (hasUDA!(this,Write)) {
-				enum varName	= __traits(identifier, var)[1..$];
-				enum varId	= getUDAs(var, Sync)[0].valueId;
-				if (bool mixin("this._networkVar_"~varId~"_changed")) {
-					msg_callback(serialize!varId);
-					mixin("this._networkVar_"~varId~"_changed") = false;
+		static foreach(var; getSymbolsByUDA!(typeof(this), Sync)) {
+			static if (hasUDA!(var,Write)) {
+				static if (hasUDA!(var,SplitArray)) {
+				}
+				else {
+					if (mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed")) {
+						msg_callback(serialize!(mixin(_networkVar_private_varId)));
+						mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed") = false;
+					}
 				}
 			}
 		}
