213968999f4dae75e970775ad2b4b8c6a4c4e77c
Unknown <JonathanILevi@gmail.com>
2018-11-16 10:33:23 -0600

    NetworkVar @SplitArray should be handling add, remove, and update events.

--- code/src/network_var_/network_var_.d
+++ code/src/network_var_/network_var_.d
@@ -5,7 +5,7 @@ enum Read	;
 enum Write	;
 enum SplitArray	;
 struct Sync {
-	uint	valueId	;
+	ubyte	valueId	;
 }
 
 template Owner(bool isOwner) {
@@ -34,15 +34,16 @@ mixin template NetworkVar() {
 		}");
  		static if (hasUDA!(var,Write)) {
 			static if (hasUDA!(var,SplitArray)) {
-				mixin("typeof(var) _networkVar_"~mixin(_networkVar_private_varIdStr)~"_added = new typeof(*var.ptr)[0];");
-				mixin("typeof(var) _networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed = new typeof(*var.ptr)[0];");
+				mixin("size_t _networkVar_"~mixin(_networkVar_private_varIdStr)~"_added = 0;");
+				mixin("size_t[] _networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed = [];");
 				@property mixin("void "~mixin(_networkVar_private_varName)~"_add(typeof(*var.ptr) n) {
 					_"~mixin(_networkVar_private_varName)~"	~= n	;
-					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_added	~= n	;
+					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_added	+= 1	;
 				}");
 				@property mixin("void "~mixin(_networkVar_private_varName)~"_remove(typeof(*var.ptr) n) {
-					_"~mixin(_networkVar_private_varName)~".remove(_"~mixin(_networkVar_private_varName)~".countUntil(n));
-					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed	~= n	;
+					size_t i = _"~mixin(_networkVar_private_varName)~".countUntil(n);
+					_"~mixin(_networkVar_private_varName)~" = _"~mixin(_networkVar_private_varName)~".remove(i);
+					_networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed	~= i;
 				}");
 			}
 			else {
@@ -62,11 +63,16 @@ mixin template NetworkVar() {
 		_networkVar_private_update(this, msg_callback, msgs);
 	}
 	static void _networkVar_private_update(typeof(this) this_, void delegate(ubyte[]) msg_callback, ubyte[][] msgs) {
+		enum Event : ubyte {
+			add	,
+			remove	,
+			update	,
+		}
 		pragma(inline, true) ubyte[] serialize(uint valueId)() {
-			return [0,1,2,5];
+			return [9,9,9,9];
 		}
 		pragma(inline, true) void deserialize(ubyte[] msg) {
-			
+			msg.log;
 		}
 		
 		foreach (msg; msgs) {
@@ -75,10 +81,25 @@ mixin template NetworkVar() {
 		static foreach(var; getSymbolsByUDA!(typeof(this), Sync)) {
 			static if (hasUDA!(var,Write)) {
 				static if (hasUDA!(var,SplitArray)) {
+					foreach (added; 0..mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_added")) {
+						msg_callback([mixin(_networkVar_private_varId), Event.add]);
+					}
+					mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_added") = 0;
+					foreach (removed; mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed")) {
+						msg_callback([mixin(_networkVar_private_varId), Event.remove, (cast(ubyte)removed)]);
+					}
+					mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_removed") = [];
+					
+					foreach (i, entity; mixin("this_._"~mixin(_networkVar_private_varName))) {
+						entity.networkVar_update((msg){
+							ubyte[] header = [mixin(_networkVar_private_varId), Event.update,(cast(ubyte)i)];
+							msg_callback(header~msg);
+						},[]);
+					}
 				}
 				else {
 					if (mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed")) {
-						msg_callback(serialize!(mixin(_networkVar_private_varId)));
+						msg_callback([mixin(_networkVar_private_varId)]~serialize!(mixin(_networkVar_private_varId)));
 						mixin("this_._networkVar_"~mixin(_networkVar_private_varIdStr)~"_changed") = false;
 					}
 				}
